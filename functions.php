43cd5fd79c5afdba33662622dfce6fc1e73b1cba
Tetsuro <tetsuro.takara@gmail.com>
2021-04-20 09:50:18 -0400

    Remove Admin Bar

--- functions.php
+++ functions.php
@@ -627,3 +627,8 @@ function twentytwentyone_add_ie_class() {
 	<?php
 }
 add_action( 'wp_footer', 'twentytwentyone_add_ie_class' );
+
+function remove_admin_login_header() {
+    remove_action('wp_head', '_admin_bar_bump_cb');
+}
+add_action('get_header', 'remove_admin_login_header');
