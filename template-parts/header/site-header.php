5a2adc8580d297b6c3e8c7e87912038a60fa45e4
Tetsuro <tetsuro.takara@gmail.com>
2021-05-03 15:12:33 -0400

    Add language picker

--- template-parts/header/site-header.php
+++ template-parts/header/site-header.php
@@ -13,6 +13,21 @@ $wrapper_classes .= ( true === get_theme_mod( 'display_title_and_tagline', true 
 $wrapper_classes .= has_nav_menu( 'primary' ) ? ' has-menu' : '';
 ?>
 
+<nav class="dal-topbar">
+	<div class="dal-topbar-container">
+		<?php wp_nav_menu(
+			array(
+				'theme_location'  => 'footer',
+				'menu_class'      => 'language-menu',
+				'container_class' => 'language-menu-container',
+				'items_wrap'      => '<ul class="%2$s">%3$s</ul>',
+				'fallback_cb'     => false,
+			)
+		);
+		?>
+	</div>
+</nav>
+
 <header id="masthead" class="<?php echo esc_attr( $wrapper_classes ); ?>" role="banner">
 
 	<?php get_template_part( 'template-parts/header/site-branding' ); ?>
