67cc784198f5a8d852c0cb8d4e7670c605bdf080
Tetsuro <tetsuro.takara@gmail.com>
2021-05-03 15:18:56 -0400

    Use Polylang script for menu output

--- template-parts/header/site-header.php
+++ template-parts/header/site-header.php
@@ -15,16 +15,7 @@ $wrapper_classes .= has_nav_menu( 'primary' ) ? ' has-menu' : '';
 
 <nav class="dal-topbar">
 	<div class="dal-topbar-container">
-		<?php wp_nav_menu(
-			array(
-				'theme_location'  => 'footer',
-				'menu_class'      => 'language-menu',
-				'container_class' => 'language-menu-container',
-				'items_wrap'      => '<ul class="%2$s">%3$s</ul>',
-				'fallback_cb'     => false,
-			)
-		);
-		?>
+		<ul class="language-menu"><?php pll_the_languages();?></ul>
 	</div>
 </nav>
 
